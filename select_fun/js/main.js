125a3d7b1d6601bf032236c78b898f7ef45fba54
Ali Pisano <pisano.alexandra@gmail.com>
2016-03-31 21:09:03 -0700

    FINAL Select Fun 1

--- select_fun/js/main.js
+++ select_fun/js/main.js
@@ -7,19 +7,29 @@
 // 1. Go to style.css and add a class called fun. Style that class with your choice of background-color and text color.
 
 // 2. Using jQuery, add the fun class to the elements with the class .box
+$('.box').addClass('.fun');
 
 // 3. When #box2 is clicked on
 	// a) Remove the crazy class from all the items with a class of .box
 	// b) Add the crazy class to the h1
 	// c) Slide toggle #box3
-
+$('#box2').on('click', function() {
+	$('.box').removeClass('crazy');
+	$('h1').addClass('crazy');
+	$('#box3').slideToggle(300);
+})
 
 // 4. When any .box is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
-
+$('.box').on('click', function() {
+	$('h1').text('jQuery Ninja');
+})
 
 // 5. Have the following code execute when the #box1 is clicked. Write a JavaScript comment with a description of what happened. 
 // $(".moto").attr("src", "images/moto.jpg");
-
+$('#box1').on('click', function() {
+	console.log('box 1 clicked');
+	$(".moto").attr("src", "images/moto.jpg");
+})
 
 
 /**
@@ -29,11 +39,13 @@
 */
 
 // 6. Hide the item with the id #dropdownMenu
-
+$('#dropdownMenu').hide(500);
 
 // 7. When the element with the id #dropdownButton is clicked,
 	// a) slide toggle #dropdownMenu
-
+$('#dropdownMenu').on('click', function() {
+	$('#dropdownMenu').slideToggle(300);
+})
 
 
 /**
@@ -43,20 +55,32 @@
 */
 
 // 8. Hide the item with the id #answer2
-
+$('#answer2').hide(500);
 
 // 9. Show the item with the id #answer1
-
+$('#answer1').show(300);
 
 // 10. When #question2 is clicked:
 	// a) Slide down #answer2
 	// b) Slide up #answer1
 	// c) Remove the active class from all list items
 	// d) Add the active class to #question2
-
+$('#question2').on('click', function(){
+	$('#answer2').slideDown();
+	$('#answer1').slideUp();
+	$('li').removeClass('.active');
+	$('#question2').addClass('.active');
+})
 
 // 11. When #question1 is clicked:
 	// a) Slide down #answer1
 	// b) Slide up #answer2
 	// c) Remove the active class from all list items
 	// d) Add the active class to #question1
+$('#question1').on('click', function(){
+	$('#answer1').slideDown();
+	$('#answer2').slideUp();
+	$('li').removeClass('.active');
+	$('#question1').addClass('.active');
+})
+
